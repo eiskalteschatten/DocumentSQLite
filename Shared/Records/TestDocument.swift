cb6cea73d2ceb3baf38d762114d5d94ad894e465
Alex Seifert <alex@alexseifert.com>
2022-05-25 14:54:27 +0200

    Add Columns

--- Shared/Records/TestDocument.swift
+++ Shared/Records/TestDocument.swift
@@ -12,6 +12,10 @@ struct TestDocument: Codable, FetchableRecord, MutablePersistableRecord {
     var id: Int64?
     var text: String
     
+    fileprivate enum Columns {
+        static let text = Column(CodingKeys.text)
+    }
+    
     mutating func didInsert(with rowID: Int64, for column: String?) {
         id = rowID
     }
