7dc527385db08a16dee9e7f6600d7a24f9f7fd4a
Alex Seifert <alex@alexseifert.com>
2022-05-25 14:08:42 +0200

    Add an ID to the model

--- Shared/Model.swift
+++ Shared/Model.swift
@@ -8,6 +8,7 @@
 import Foundation
 import GRDB
 
-struct TextModel: Codable, FetchableRecord, PersistableRecord {
+struct TextModel: Codable, FetchableRecord, MutablePersistableRecord {
+    var id: Int64?
     var text: String
 }
